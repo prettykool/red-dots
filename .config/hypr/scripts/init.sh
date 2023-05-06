eea80b23553e255e5279bb60d7fa743e1f636f37
PK <prettykool@protonmail.com>
2023-05-05 20:19:53 -0500

    add crap

---
+++ .config/hypr/scripts/init.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+# Used to activate some daemons, and get everything else set up, basically.
+swww init 
+swww img ../geo-bg-material.png
+mpd
+foot -s
---
+++ readme.md
@@ -0,0 +1,2 @@
+# red dotfiles
+A really red colorscheme for a ricing competition on Hyprlands Discord server.
